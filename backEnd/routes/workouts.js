2cf3ae177d14453808365f8dbf99ad3727ea3738
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-04-13 21:23:13 +0200

    added put method

--- backEnd/routes/workouts.js
+++ backEnd/routes/workouts.js
@@ -37,13 +37,24 @@ let workout = new Workout({
   res.send(workout);
 });
 
-router.delete('/', async (req, res) => {
-    const workout = await Workout.findByIdAndRemove(req.body._id);
-  
-    if (!workout) return res.status(404).send('The customer with the given ID was not found.');
-  
-    res.send(workout);
-  
+router.put('/', async (req, res) => {
+    
+/*   const { error } = validateUpdate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const emailCheck = await Customer.findOne({email: req.body.email});
+  if (emailCheck) return res.status(400).send('Email already registered'); */
+
+  const workout = await Workout.findByIdAndUpdate(req.body._id, { 
+    workout: req.body.workout,
+    notes: req.body.notes,
+    duration: req.body.duration }, {
+    new: true
   });
+
+  if (!workout) return res.status(404).send('The workout with the given ID was not found.');
+  
+  res.send(workout);
+});
 
 module.exports = router;

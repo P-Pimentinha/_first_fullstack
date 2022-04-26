69164ee559f3d105fa98eca8e4f73d3b3f9ea777
SeanPaulson <spausm1@gmail.com>
2022-04-26 01:05:22 -0600

    added react-bootstrap and upgraded react to V 18

--- workout-tracker/src/App.js
+++ workout-tracker/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col>col 1 of 2</Col>
+                <Col>col 2 of 2</Col>
+            </Row>
+            <Row>
+                <Col>col2 1 of 2</Col>
+                <Col>col2 2 of 2</Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default App;
